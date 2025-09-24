8442ff33517a25d685c95d3ffd7a73adc71b049d
Dustin Loring <agent.cli.robot@gmail.com>
2025-09-23 21:39:46 -0400

    chore: fixed use of deafult qwen model

--- packages/core/src/config/models.ts
+++ packages/core/src/config/models.ts
@@ -8,7 +8,7 @@ export const DEFAULT_QWEN_MODEL = 'qwen3-coder-plus';
 // We do not have a fallback model for now, but note it here anyway.
 export const DEFAULT_QWEN_FLASH_MODEL = 'qwen3-coder-flash';
 
-export const DEFAULT_GEMINI_MODEL = 'qwen3-coder-plus'; // Change back to 'gemini-2.5-pro'
+export const DEFAULT_GEMINI_MODEL = 'gemini-2.5-pro';
 export const DEFAULT_GEMINI_FLASH_MODEL = 'gemini-2.5-flash';
 export const DEFAULT_GEMINI_FLASH_LITE_MODEL = 'gemini-2.5-flash-lite';
 
