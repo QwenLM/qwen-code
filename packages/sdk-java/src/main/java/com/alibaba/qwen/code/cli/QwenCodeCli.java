37f5eb4f305186954317749d64758b9eebd4f62f
zzwcodegithub <2207692863@qq.com>
2026-01-10 23:45:47 +0800

    modify some fields

--- packages/sdk-java/src/main/java/com/alibaba/qwen/code/cli/QwenCodeCli.java
+++ packages/sdk-java/src/main/java/com/alibaba/qwen/code/cli/QwenCodeCli.java
@@ -7,7 +7,7 @@ import com.alibaba.fastjson2.JSON;
 import com.alibaba.qwen.code.cli.protocol.data.AssistantUsage;
 import com.alibaba.qwen.code.cli.protocol.data.AssistantContent;
 import com.alibaba.qwen.code.cli.protocol.data.AssistantContent.TextAssistantContent;
-import com.alibaba.qwen.code.cli.protocol.data.AssistantContent.ThingkingAssistantContent;
+import com.alibaba.qwen.code.cli.protocol.data.AssistantContent.ThinkingAssistantContent;
 import com.alibaba.qwen.code.cli.protocol.data.AssistantContent.ToolResultAssistantContent;
 import com.alibaba.qwen.code.cli.protocol.data.AssistantContent.ToolUseAssistantContent;
 import com.alibaba.qwen.code.cli.protocol.data.behavior.Behavior.Operation;
@@ -59,7 +59,7 @@ public class QwenCodeCli {
             }
 
             @Override
-            public void onThinking(Session session, ThingkingAssistantContent thingkingAssistantContent) {
+            public void onThinking(Session session, ThinkingAssistantContent thingkingAssistantContent) {
                 response.add(thingkingAssistantContent.getThinking());
             }
 
--- packages/sdk-java/src/test/java/com/alibaba/qwen/code/cli/example/SessionExample.java
+++ packages/sdk-java/src/test/java/com/alibaba/qwen/code/cli/example/SessionExample.java
@@ -4,7 +4,7 @@ import com.alibaba.fastjson2.JSON;
 import com.alibaba.qwen.code.cli.QwenCodeCli;
 import com.alibaba.qwen.code.cli.protocol.data.AssistantContent;
 import com.alibaba.qwen.code.cli.protocol.data.AssistantContent.TextAssistantContent;
-import com.alibaba.qwen.code.cli.protocol.data.AssistantContent.ThingkingAssistantContent;
+import com.alibaba.qwen.code.cli.protocol.data.AssistantContent.ThinkingAssistantContent;
 import com.alibaba.qwen.code.cli.protocol.data.AssistantContent.ToolResultAssistantContent;
 import com.alibaba.qwen.code.cli.protocol.data.AssistantContent.ToolUseAssistantContent;
 import com.alibaba.qwen.code.cli.protocol.data.AssistantUsage;
@@ -219,7 +219,7 @@ public class SessionExample {
                 }
 
                 @Override
-                public void onThinking(Session session, ThingkingAssistantContent thingkingAssistantContent) {
+                public void onThinking(Session session, ThinkingAssistantContent thingkingAssistantContent) {
                     logger.info("Received thingkingAssistantContent {}", thingkingAssistantContent.getThinking());
                 }
 
