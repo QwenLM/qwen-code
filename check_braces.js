39c54d00071e88d8fee0ea350364097c7aed31b1
lamb <906276457@qq.com>
2026-04-19 18:58:44 +0800

    chore: remove debug script check_braces.js

--- check_braces.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-const c = fs.readFileSync('packages/core/src/utils/editor.test.ts', 'utf8');
-let d = 0, o = 0;
-for (let i = 0; i < c.length; i++) {
-  if (c[i] === '{') d++;
-  if (c[i] === '}') o++;
-}
-console.log('opens:', d, 'closes:', o, 'diff:', d - o);
