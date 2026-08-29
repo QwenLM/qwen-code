e716593ebff45caa6af196263c8665361be6f707
俊良 <zzj542558@alibaba-inc.com>
2026-08-29 15:49:15 +0800

    test(web-shell): relax slash refresh serialization timeout

--- packages/web-shell/client/hooks/useComposerCore.dom.test.tsx
+++ packages/web-shell/client/hooks/useComposerCore.dom.test.tsx
@@ -283,7 +283,7 @@ describe('useComposerCore history and drafts', () => {
     );
 
     expect(toString).toHaveBeenCalledOnce();
-  });
+  }, 10_000);
 
   it('keeps slash completion replacement coordinates absolute', async () => {
     await mount({
